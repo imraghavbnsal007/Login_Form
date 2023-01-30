3a434290023dcb9b02a6c13b89b22e41c54f97bc
imraghavbnsal007 <raghavb025@gmail.com>
2023-01-30 11:36:54 +0200

    Login Form

--- app/src/main/java/com/example/login_form/MainActivity.kt
+++ app/src/main/java/com/example/login_form/MainActivity.kt
@@ -3,13 +3,25 @@ package com.example.login_form
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.login_form.ui.theme.Login_FormTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    LoginForm()
                 }
             }
         }
@@ -30,14 +42,53 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun LoginForm()
+{
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var passwordVisible: Boolean by remember { mutableStateOf(false) }
+    val reusableModifier = Modifier
+        .fillMaxWidth()
+        .padding(8.dp)
+    Column(
+
+    )
+    {
+        Text(
+            text = "Login",
+            modifier = reusableModifier,
+            fontSize= 40.sp,
+            color = MaterialTheme.colors.primary
+        )
+        OutlinedTextField(
+            value = username,
+            onValueChange = {username = it},
+            label = { Text(text = "Username")},
+            modifier = reusableModifier,
+            trailingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "Email icon") }
+        )
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it},
+            label = { Text(text = "Password")},
+            modifier = reusableModifier,
+            singleLine = true,
+            visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            trailingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = "Lock icon") }
+        )
+        Button(onClick = { /*TODO*/ }, modifier = reusableModifier) {
+            Text(text = "Submit")
+            
+        }
+        
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Login_FormTheme {
-        Greeting("Android")
+        LoginForm()
     }
 }
